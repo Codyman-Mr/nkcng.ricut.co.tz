aa63bfa64b2890b99abdacd9dd334802afdf0c82
michaelmgondasr <mgondaemmanuel@gmail.com>
2025-05-07 04:39:06 +0300

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+const env = dotenv.parse(fs.readFileSync('/home/ec2-user/nkcng-2/.env'));
 
 export default defineConfig({
     plugins: [
@@ -9,9 +13,9 @@ export default defineConfig({
         }),
     ],
     define: {
-        'import.meta.env.VITE_REVERB_APP_KEY': JSON.stringify(process.env.VITE_REVERB_APP_KEY),
-        'import.meta.env.VITE_REVERB_HOST': JSON.stringify(process.env.VITE_REVERB_HOST),
-        'import.meta.env.VITE_REVERB_PORT': JSON.stringify(process.env.VITE_REVERB_PORT),
-        'import.meta.env.VITE_REVERB_SCHEME': JSON.stringify(process.env.VITE_REVERB_SCHEME),
+        'import.meta.env.VITE_REVERB_APP_KEY': JSON.stringify(env.VITE_REVERB_APP_KEY),
+        'import.meta.env.VITE_REVERB_HOST': JSON.stringify(env.VITE_REVERB_HOST),
+        'import.meta.env.VITE_REVERB_PORT': JSON.stringify(env.VITE_REVERB_PORT),
+        'import.meta.env.VITE_REVERB_SCHEME': JSON.stringify(env.VITE_REVERB_SCHEME),
     },
 });
