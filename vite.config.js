d4f69508b3db6f0fd1ecdad59004ee47fe3bcfde
michaelmgondasr <mgondaemmanuel@gmail.com>
2025-05-07 22:19:59 +0300

    refactor: update build configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
     build: {
         manifest: true, // Required for Laravel to find assets in production
         outDir: 'public/build', // Default; just to be explicit
+        emptyOutDir: true, // Clean the output directory before building
+        sourcemap: false, // Disable source maps for production
+        minify: 'terser', // Use Terser for minification
         rollupOptions: {
             input: {
                 app: 'resources/js/app.js',
